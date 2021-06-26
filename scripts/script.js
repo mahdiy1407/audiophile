a59279e1a03cf51dda1a2a3473434cdb6dd7eccc
mahdiy_1407 <mahdiysulaymonov66@gmail.com>
2021-06-26 10:06:46 -0700

    big changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,6 @@
-console.log('hey!');
+var shop = document.querySelector(".shop");
+var btn = document.querySelector(".checkout")
+
+shop.addEventListener("click", function () {
+    btn.classList.toggle("checkout--active");
+});
